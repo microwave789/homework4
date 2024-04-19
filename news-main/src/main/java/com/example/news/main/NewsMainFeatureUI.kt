8471a78648f0a0239921c726eb84bd68b3c103cf
microwave789 <merei.zhazitova@gmail.com>
2024-04-20 01:06:43 +0500

    fix bugs of images

--- news-main/src/main/java/com/example/news/main/NewsMainFeatureUI.kt
+++ news-main/src/main/java/com/example/news/main/NewsMainFeatureUI.kt
@@ -3,11 +3,14 @@ package com.example.news.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -17,14 +20,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import com.example.news.uikit.NewsTheme
 
 @Composable
@@ -94,7 +101,7 @@ private fun ProgressIndicator(state: State.Loading){
     private fun Articles(
     @PreviewParameter(ArticlesPreviewProvider::class, limit = 1)articles: List<ArticleUI>,
     ){
-        LazyColumn {
+        LazyColumn{
            items(articles){article ->
                key(article.id){
                         Article(article = article)
@@ -112,13 +119,23 @@ private fun ProgressIndicator(state: State.Loading){
  internal fun Article(
     @PreviewParameter(ArticlePreviewProvider::class, limit = 1)article: ArticleUI,
  ) {
-     Row {
+     Row(Modifier.padding(bottom = 4.dp)) {
 
              article.imageUrl?.let { imageUrl ->
-                 AsyncImage(
-                     model = article.imageUrl,
-                     contentDescription = stringResource(R.string.content_desc_item_article_image)
-                 )
+                 var isImageVisible by mutableStateOf(true)
+                 if(isImageVisible) {
+                     AsyncImage(
+                         model = article.imageUrl,
+                         onState = {state ->
+                             if (state is AsyncImagePainter.State.Error){
+                                 isImageVisible = false
+                             }
+                         },
+                         contentDescription = stringResource(R.string.content_desc_item_article_image),
+                         contentScale = ContentScale.Crop,
+                         modifier = Modifier.size(150.dp)
+                     )
+                 }
              }
              Spacer(modifier = Modifier.size(4.dp))
              Column(modifier = Modifier.padding(8.dp)){
