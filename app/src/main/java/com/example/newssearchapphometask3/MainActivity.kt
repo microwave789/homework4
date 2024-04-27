40468b7a91dc49c4d29d43d7d54a1fa88d02439f
microwave789 <merei.zhazitova@gmail.com>
2024-04-27 11:51:19 +0500

    fix according to detekt

--- app/src/main/java/com/example/newssearchapphometask3/MainActivity.kt
+++ app/src/main/java/com/example/newssearchapphometask3/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.newssearchapphometask3
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,15 +9,12 @@ import androidx.compose.ui.Modifier
 import com.example.news.main.NewsMainScreen
 import com.example.news.uikit.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
-
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NewsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
